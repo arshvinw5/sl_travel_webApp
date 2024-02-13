95884539077df3e7bdad51cf4b106558c0d5d210
Theonaw4 <tarshvin@gmail.com>
2024-02-14 01:53:30 +0530

    clean code the footer with map

--- src/app/_components/header/navbar/button/footer/footer.tsx
+++ src/app/_components/header/navbar/button/footer/footer.tsx
@@ -1,38 +1,27 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import styles from './styles.module.scss';
-import useAnim from '../../useAnim';
 
 import { motion } from 'framer-motion';
 import { socialMediaIcons } from './iconArr';
 import { slideFooter } from '../../animation';
-import { SocialMediaIcon } from '@/app/_dto/navbar_footer_dto';
 
 const Footer = () => {
-	const getIcon = (icon: SocialMediaIcon[]) => {
-		let iconElements: JSX.Element[] = [];
-		icon.forEach(({ icon: Icon, href }, i) => {
-			iconElements.push(
-				<motion.div
-					key={`icon_${i}`}
-					custom={i}
-					variants={slideFooter}
-					initial='initial'
-					animate='enter'
-					exit='exit'
-				>
-					<a href={href}>
-						<Icon />
-					</a>
-				</motion.div>
-			);
-		});
-		return iconElements;
-	};
-	return (
-		<motion.div className={styles.footer}>
-			{getIcon(socialMediaIcons)}
-		</motion.div>
-	);
+	const iconElements = socialMediaIcons.map(({ href, icon: Icon, name }, i) => {
+		return (
+			<motion.div
+				key={`icon_${i}`}
+				custom={i}
+				variants={slideFooter}
+				initial='initial'
+				animate='enter'
+				exit='exit'
+			>
+				<a href={href}>
+					<Icon />
+				</a>
+			</motion.div>
+		);
+	});
+	return <div className={styles.footer}>{iconElements}</div>;
 };
 
 export default Footer;
